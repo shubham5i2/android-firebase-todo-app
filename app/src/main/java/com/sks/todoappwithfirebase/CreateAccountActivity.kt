67d0db1089bdfdc8a0b75ad8171173bad272da60
shubham5i2 <shubhamkumarsingh450@gmail.com1>
2024-02-21 22:45:37 +0530

    create account ui changes

--- app/src/main/java/com/sks/todoappwithfirebase/CreateAccountActivity.kt
+++ app/src/main/java/com/sks/todoappwithfirebase/CreateAccountActivity.kt
@@ -1,6 +1,7 @@
 package com.sks.todoappwithfirebase
 
 import android.os.Bundle
+import android.util.Patterns
 import androidx.appcompat.app.AppCompatActivity
 import com.sks.todoappwithfirebase.databinding.ActivityCreateAccountBinding
 
@@ -13,5 +14,46 @@ class CreateAccountActivity : AppCompatActivity() {
         createAccountBinding = ActivityCreateAccountBinding.inflate(layoutInflater)
         val view = createAccountBinding.root
         setContentView(view)
+
+        createAccountBinding.createAccountBtn.setOnClickListener {
+            createAccount()
+        }
+
+        createAccountBinding.loginTextViewBtn.setOnClickListener {
+            finish()
+        }
+    }
+
+    private fun createAccount() {
+        val fullName = createAccountBinding.fullnameEditText.text.toString()
+        val email = createAccountBinding.emailEditText.text.toString()
+        val password = createAccountBinding.passwordEditText.text.toString()
+        val confirmPassword = createAccountBinding.confirmPasswordEditText.text.toString()
+
+        val isValidated = validateData(fullName, email, password, confirmPassword)
+
+    }
+
+    private fun validateData(
+        fullName: String, email: String, password: String, confirmPassword: String
+    ): Boolean {
+        if (fullName.trim().equals("")) {
+            createAccountBinding.fullnameEditText.error = "Full name is required"
+            return false
+        }
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            createAccountBinding.emailEditText.error = "Please enter a valid email address"
+            return false
+        }
+        if (password.length < 6) {
+            createAccountBinding.passwordEditText.error =
+                "Password length should be minimum 6 characters"
+            return false
+        }
+        if (!password.equals(confirmPassword)) {
+            createAccountBinding.confirmPasswordEditText.error = "Passwords does not match"
+            return false
+        }
+        return true
     }
 }
